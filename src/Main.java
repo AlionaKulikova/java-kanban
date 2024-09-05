6b64c3066d4ed72a210f3ed888ef08e0aaa109e1
Aliona Kulikova <kulikovaaliona888@yandex.ru>
2024-09-05 20:57:30 +0300

    пробный коммит

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import tasks.SubTask;
 import tasks.Task;
 
 class Main {
-
+    g
     public static void main(String[] args) {
         TaskManager manager = Managers.getInMemoryTaskManager(Managers.getDefaultHistory());
 
